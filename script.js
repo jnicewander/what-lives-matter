6464c6b26967049567be941838ca1a43002cf619
J.Nicewander <jnicewander@live.com>
2020-06-24 16:56:25 -0400

    Meaningless site.

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('findOut').addEventListener('click', function() {
+    const initialState = document.getElementById('initial');
+    const afterState = document.getElementById('after');
+    const initialChilds = initialState.children;
+    const afterChilds = afterState.children;
+
+    initialState.style.display = 'none';
+    for(let elements of initialChilds) {
+       elements.style.display = 'none';
+    }
+
+    afterState.style.display = 'block';
+    document.body.style.background
+    document.body.style.backgroundImage = "url('flames.png')";
+    for(let elements of afterChilds) {
+       elements.style.display = 'initial';
+    }
+})
