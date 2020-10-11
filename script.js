0725b7c83776dfabcc760c9793437af9964575bf
jnicewander <jnicewander@live.com>
2020-10-11 11:36:58 -0400

    Randomly inserts names based on the size of view.

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 import {victims} from './formattedData.js';
 
+window.addEventListener('resize', randomFill);
 const spanContainer = document.getElementById('span-container');
+const victimNames = victims.map(({name, died}) => ({name, died}));
+let tracker = new Set();
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerHeight || 0);
+const ulSize = {
+    height: function() {
+        return spanContainer.offsetHeight;
+    },
+    width: function() {
+        return spanContainer.offsetWidth;
+    }
+}
 
-function colorize(array, index) {
-    let lightness = 100;
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function colorize(date) {
+    let lightness = date.includes('2020') ? 50 : 100;
     let color = `hsl(4, 100%, ${lightness}%)`;
-    
     return color;
 }
 
-function fillContainer() {
-    victims.forEach((victim, index) => {
-        const span = document.createElement('span');
-        span.style.color = colorize(index);
-        span.innerText = ` ${victim.name.toUpperCase()} `;
-        spanContainer.appendChild(span);
-    })
-};
+function randomName() { 
+    let index = null;
+    index = Math.floor(Math.random() * victimNames.length);
+    return victimNames[index];
+}
 
-fillContainer();
+function randomFill() {
+    removeAllChildren(spanContainer);
+    let childHeight = 0;
+    while (childHeight <= vh + ((vh * 25) / 100)) {
+        let victim = randomName();
+        if (victim) {
+            const span = document.createElement('span');
+            span.style.color = colorize(victim.died);
+            span.innerText = ` ${victim.name} `;
+            spanContainer.appendChild(span);
+            childHeight = ulSize.height();
+        }
+    }
+}
+
+randomFill();
